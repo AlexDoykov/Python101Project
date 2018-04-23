ffba341faee98aefabd58f7a51e2d67ac839bc12
Alexander <doykov70@gmail.com>
2018-04-23 20:54:30 +0300

    added verification for directions

---
+++ verify.py
@@ -0,0 +1,18 @@
+def varify_int(arg):
+    if type(arg) is not int:
+        raise TypeError("Int expected")
+    else:
+        return True
+
+
+def varify_positive(arg):
+    if arg < 0:
+        raise ValueError("Positive expected")
+    else:
+        return True
+
+
+def verify_direction(direction):
+    if direction == 'up' or direction == 'down' or direction == 'left' or direction == 'down':
+        return True
+    raise ValueError("Wrong direction")
